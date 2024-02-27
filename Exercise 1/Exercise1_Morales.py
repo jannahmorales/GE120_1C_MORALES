f85a0fb98a412729d4d07746465e7d356e6f2024
Jannah Marc D. Morales <jdmorales3@up.edu.ph>
2024-02-27 17:41:35 +0800

    Exercise 1 Submission

--- Exercise 1/Exercise1_Morales.py
+++ Exercise 1/Exercise1_Morales.py
@@ -1,6 +1,38 @@
 """
-GE 12O 
+Exercise 1
 Jannah Marc Morales
 2023-03947
 """
 
+# Input Decimal Degrees
+DD = 120.90347
+print("Input (In Decimal Degrees):", DD)
+
+# Get degrees
+D = int(DD)
+# Get minutes (integer) and minutes (whole)
+M = (DD - D)*60
+M_WHOLE = int(M)
+# Get seconds
+S = (M-M_WHOLE)*60
+# Result, Print Output
+DMS = [D, M, S]
+
+print("DMS:" + str(D) + "-" + str(M_WHOLE) + "-" + str(round(S, 2)))
+print() #Separator for Part 1 and Part 2
+
+#PART2
+
+# Input DD
+dms = "120-54-12.49"
+values = dms.split("-")
+print("Input (In Degrees-Minutes-Seconds):", dms)
+
+# Converting string to integers and float data 
+degrees = int(values[0])
+minutes = int(values[1])
+seconds = float(values[2])
+
+# Conversion into DD
+dd = degrees + (minutes/60) + (seconds/3600)
+print("DD:", round(dd, 6))
